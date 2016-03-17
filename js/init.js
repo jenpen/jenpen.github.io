610787eb7a1bcb7e917ad0488a0dc21733a59573
jen pengelly <jgpengelly@gmail.com>
2016-03-17 11:37:39 -0500

    changes to init file

--- js/init.js
+++ js/init.js
@@ -7,5 +7,9 @@
        }
      );    $('.parallax').parallax();
 
+       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+       $('.modal-trigger').leanModal();
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
